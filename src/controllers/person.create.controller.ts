cdf72cb6f889984bce57671590bf0733f7ca1c0d
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-12-14 15:02:51 -0300

    modify create person controler

--- src/controllers/person.create.controller.ts
+++ src/controllers/person.create.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { PersonModel } from "src/models/person.model";
 
 @Controller('/person')
 export class PersonCreateController {
 
+    constructor(@InjectRepository(PersonModel) private model: Repository<PersonModel>) { }
+
     @Post()
-    public create(): any {
+    public create(@Body() body: PersonSchema): any {
         return {
             data: ''
         }
