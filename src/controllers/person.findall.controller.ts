17c439ae92dd4e15887ea050aa9da5389ea04973
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-12-14 15:02:32 -0300

    modify find all person controller

--- src/controllers/person.findall.controller.ts
+++ src/controllers/person.findall.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { PersonModel } from "src/models/person.model";
+import { Repository } from "typeorm";
 
 @Controller('/person')
 export class PersonFindAllController {
-    
+
+    constructor(@InjectRepository(PersonModel) private model: Repository<PersonModel>) { }
+
     @Get()
-    public findAll(): any {
+    public async findAll(): Promise<{data: PersonModel[]}> {
+        const list = await this.model.find();
         return {
-            data: ''
+            data: list
         }
     }
 }
