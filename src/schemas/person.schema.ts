0d2c05435a7a7afdfa57039702138a929cdd8f67
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-12-15 10:50:20 -0300

    add person schema for validation

--- src/schemas/person.schema.ts
+++ src/schemas/person.schema.ts
@@ -1,5 +1,16 @@
+import { IsEmail, IsString, MaxLength } from "class-validator"
+
 export class PersonSchema {
+    @IsString()
+    @MaxLength(100)
     public name: string
+
+    @IsString()
+    @MaxLength(100)
+    @IsEmail()
     public email: string
+
+    @IsString()
+    @MaxLength(16)
     public password: string
 }
