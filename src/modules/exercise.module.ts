2720c4daf2acc0907c82f30908a4b9fbfeaaed8b
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-12-17 20:45:17 -0300

    add exercise module class

---
+++ src/modules/exercise.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ExerciseCreateController } from "src/controllers/exercise.create.controller";
+import { ExerciseDeleteController } from "src/controllers/exercise.delete.controller";
+import { ExerciseFindController } from "src/controllers/exercise.find.controller";
+import { ExerciseFindAllController } from "src/controllers/exercise.findall.controller";
+import { BodyBuildingExercise } from "src/models/bodybuildingexercise.model";
+import { CardioExercise } from "src/models/cardioexercise.model";
+
+@Module({
+    imports: [TypeOrmModule.forFeature([BodyBuildingExercise, CardioExercise])],
+    controllers: [ExerciseCreateController, ExerciseFindAllController, ExerciseFindController,
+        ExerciseDeleteController
+    ]
+})
+export class ExerciseModule {}
