eeb5a90e4d668e134df9778a7f323dfc69b68c13
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-12-17 20:44:57 -0300

    add exercise module to imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PersonModule } from './modules/person.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ExerciseModule } from './modules/exercise.module';
 
 @Module({
-  imports: [PersonModule, TypeOrmModule.forRoot({
+  imports: [PersonModule, ExerciseModule, TypeOrmModule.forRoot({
     "database": "./database.sql",
     "type": "sqlite",
     "synchronize": true,
