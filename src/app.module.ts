1a086a10a0e7b1feac311a1ec249558c37a77847
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2024-12-18 11:18:05 -0300

    add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,20 @@ import { Module } from '@nestjs/common';
 import { PersonModule } from './modules/person.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ExerciseModule } from './modules/exercise.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [PersonModule, ExerciseModule, TypeOrmModule.forRoot({
-    "database": "./database.sql",
-    "type": "sqlite",
-    "synchronize": true,
-    "entities": ["dist/**/*.model.js"]
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    synchronize: true,
+    entities: ["dist/**/*.model.js"],
   })],
 })
 export class AppModule { }
