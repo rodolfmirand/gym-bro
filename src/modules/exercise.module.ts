22f43a55638e5ad990d260b50378330d636c2dbc
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2024-12-18 10:32:21 -0300

    add exercise update controller to imports

--- src/modules/exercise.module.ts
+++ src/modules/exercise.module.ts
@@ -4,13 +4,14 @@ import { ExerciseCreateController } from "src/controllers/exercise.create.contro
 import { ExerciseDeleteController } from "src/controllers/exercise.delete.controller";
 import { ExerciseFindController } from "src/controllers/exercise.find.controller";
 import { ExerciseFindAllController } from "src/controllers/exercise.findall.controller";
+import { ExerciseUpdateController } from "src/controllers/exercise.update.controller";
 import { BodyBuildingExercise } from "src/models/bodybuildingexercise.model";
 import { CardioExercise } from "src/models/cardioexercise.model";
 
 @Module({
     imports: [TypeOrmModule.forFeature([BodyBuildingExercise, CardioExercise])],
     controllers: [ExerciseCreateController, ExerciseFindAllController, ExerciseFindController,
-        ExerciseDeleteController
+        ExerciseDeleteController, ExerciseUpdateController
     ]
 })
 export class ExerciseModule {}
