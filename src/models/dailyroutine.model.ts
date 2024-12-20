c20c411686dcdbc6aea52da50363a1bf0e7731a8
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2024-12-20 14:48:32 -0300

    add constructor and name atribute

--- src/models/dailyroutine.model.ts
+++ src/models/dailyroutine.model.ts
@@ -1,19 +1,27 @@
-import { Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn, Unique } from "typeorm";
 import { CardioExercise } from "./cardioexercise.model";
 import { BodyBuildingExercise } from "./bodybuildingexercise.model";
 
 @Entity()
+@Unique(["name"])
 export class DailyRoutine {
-    
+
     @PrimaryGeneratedColumn('uuid')
-    private id: string
+    id: string
+
+    @Column({ length: 10 })
+    name: string
 
     @ManyToMany(type => CardioExercise)
     @JoinTable()
-    private cardioExercises: CardioExercise
+    cardioExercises: CardioExercise[]
 
     @ManyToMany(type => BodyBuildingExercise)
     @JoinTable()
-    private bodybuildingExercises: CardioExercise
+    bodybuildingExercises: CardioExercise[]
+
+    constructor(name: string) {
+        this.name = name
+    }
 
 }
