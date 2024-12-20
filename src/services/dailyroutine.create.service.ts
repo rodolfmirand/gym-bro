6be1d2182dce3b4a9ac9372532a8fc7c0020e47e
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2024-12-20 14:43:57 -0300

    add daily routine services

---
+++ src/services/dailyroutine.create.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { DailyRoutine } from "src/models/dailyroutine.model";
+import { Repository } from "typeorm";
+import { WorkoutRoutineFindByPersonIdService } from "./workoutroutine.findbypersonid.service";
+import { WorkoutRoutineAddDailyRoutineService } from "./workoutroutine.adddailyroutine.service";
+import { WorkoutRoutineUpdateService } from "./workoutroutine.update.service";
+
+@Injectable()
+export class DailyRoutineCreateService {
+
+    constructor(@InjectRepository(DailyRoutine) private model: Repository<DailyRoutine>,
+        private readonly workoutUpdateService: WorkoutRoutineUpdateService,
+        private readonly workoutFindByPersonIdService: WorkoutRoutineFindByPersonIdService,
+        private readonly workoutAddDailyRoutineService: WorkoutRoutineAddDailyRoutineService) { }
+
+    public async create(body: DailyRoutine, id: string): Promise<DailyRoutine> {
+        const workout = await this.workoutFindByPersonIdService.findByPersonId(id)
+        const dailyRoutine = new DailyRoutine(body.name)
+        await this.model.save(dailyRoutine)
+        this.workoutAddDailyRoutineService.add(dailyRoutine, workout)
+        this.workoutUpdateService.update(id, workout)
+        return dailyRoutine
+    }
+}
---
+++ src/services/dailyroutine.findall.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { DailyRoutine } from "src/models/dailyroutine.model";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class DailyRoutineFindAllService {
+
+    constructor(@InjectRepository(DailyRoutine) private model: Repository<DailyRoutine>) { }
+
+    public async findAll(): Promise<DailyRoutine[]> {
+        return this.model.find()
+    }
+
+}
