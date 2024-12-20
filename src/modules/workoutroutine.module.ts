984c9e000ceccf00ad248a72fe9e7b2ad38af85b
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2024-12-20 10:00:09 -0300

    add person modules import

--- src/modules/workoutroutine.module.ts
+++ src/modules/workoutroutine.module.ts
@@ -3,12 +3,15 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { WorkoutRoutineCreateController } from "src/controllers/workoutroutine.create.controller";
 import { WorkoutRoutineFindAllController } from "src/controllers/workoutroutine.findall.controller";
 import { WorkoutRoutine } from "src/models/workoutroutine.model";
+import { PersonModule } from "./person.module";
 import { WorkoutRoutineCreateService } from "src/services/workoutroutine.create.service";
 import { WorkoutRoutineFindAllService } from "src/services/workoutroutine.findall.service";
+import { WorkoutRoutineFindByPersonIdService } from "src/services/workoutroutine.findbypersonid.service";
+import { WorkoutRoutineFindByPersonIdController } from "src/controllers/workoutroutine.findbypersonid.controller";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([WorkoutRoutine])],
-    controllers: [WorkoutRoutineCreateController, WorkoutRoutineFindAllController],
-    providers: [WorkoutRoutineCreateService, WorkoutRoutineFindAllService]
+    imports: [TypeOrmModule.forFeature([WorkoutRoutine]), PersonModule],
+    controllers: [WorkoutRoutineCreateController, WorkoutRoutineFindAllController, WorkoutRoutineFindByPersonIdController],
+    providers: [WorkoutRoutineCreateService, WorkoutRoutineFindAllService, WorkoutRoutineFindByPersonIdService]
 })
 export class WorkoutRoutineModule { }
