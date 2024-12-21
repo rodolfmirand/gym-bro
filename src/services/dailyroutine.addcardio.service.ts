e536ae323231bc90ebf456a1adc981cff26d1400
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-12-20 21:55:13 -0300

    add daily routine add cardio service

---
+++ src/services/dailyroutine.addcardio.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { CardioExercise } from "src/models/cardioexercise.model";
+import { CardioCreateService } from "./cardio.create.service";
+import { WorkoutRoutineUpdateService } from "./workoutroutine.update.service";
+import { DailyRoutineFindByWorkoutIdService } from "./dailyroutine.findbyworkoutid.service";
+import { DailyRoutineUpdateService } from "./dailyroutine.update.service";
+
+
+@Injectable()
+export class DailyRoutineAddCardioService {
+
+    constructor(private readonly dailyRoutineFindByWorkoutId: DailyRoutineFindByWorkoutIdService,
+        private readonly cardioCreateService: CardioCreateService,
+        private readonly workoutUpdateService: WorkoutRoutineUpdateService,
+        private readonly dailyRoutineUpdateService: DailyRoutineUpdateService) { }
+
+    public async add(id: string, idDay: string, body: CardioExercise) {
+        const dailyRoutine = await this.dailyRoutineFindByWorkoutId.find(id)
+        const cardio = await this.cardioCreateService.create(body)
+        const day = dailyRoutine.find((day) => day.id == idDay)
+        if (!day)
+            throw new NotFoundException('Day not found')
+        day.cardioExercises.push(cardio)
+        await this.dailyRoutineUpdateService.update(dailyRoutine)
+        return cardio
+    }
+}
