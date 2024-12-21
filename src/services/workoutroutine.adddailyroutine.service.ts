7e47f8e6f09edc56a075f18450015f3189f77227
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-12-20 22:04:03 -0300

    modify push

--- src/services/workoutroutine.adddailyroutine.service.ts
+++ src/services/workoutroutine.adddailyroutine.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
 import { DailyRoutine } from "src/models/dailyroutine.model";
 import { WorkoutRoutine } from "src/models/workoutroutine.model";
-import { Repository } from "typeorm";
 
 @Injectable()
 export class WorkoutRoutineAddDailyRoutineService {
 
-    public async add(routine: DailyRoutine, workout: WorkoutRoutine) {
+    public async add(dailyRoutine: DailyRoutine, workout: WorkoutRoutine) {
         try {
-            workout.dailyRoutine.push(routine)
+            workout.dailyRoutine.push(dailyRoutine)
         } catch (error) {
-            console.log('An error ocurred: ', error)
+            throw error
         }
     }
 }
