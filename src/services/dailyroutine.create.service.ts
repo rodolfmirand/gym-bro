50d6b086370873c8e96aaf9784ba30cd2beec206
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-12-23 16:22:45 -0300

    modify id request -> workout routine id now

--- src/services/dailyroutine.create.service.ts
+++ src/services/dailyroutine.create.service.ts
@@ -2,20 +2,20 @@ import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { DailyRoutine } from "src/models/dailyroutine.model";
 import { Repository } from "typeorm";
-import { WorkoutRoutineFindByPersonIdService } from "./workoutroutine.findbypersonid.service";
 import { WorkoutRoutineAddDailyRoutineService } from "./workoutroutine.adddailyroutine.service";
 import { WorkoutRoutineUpdateService } from "./workoutroutine.update.service";
+import { WorkoutRoutineFindService } from "./workoutroutine.find.service";
 
 @Injectable()
 export class DailyRoutineCreateService {
 
     constructor(@InjectRepository(DailyRoutine) private model: Repository<DailyRoutine>,
         private readonly workoutUpdateService: WorkoutRoutineUpdateService,
-        private readonly workoutFindByPersonIdService: WorkoutRoutineFindByPersonIdService,
-        private readonly workoutAddDailyRoutineService: WorkoutRoutineAddDailyRoutineService) { }
+        private readonly workoutAddDailyRoutineService: WorkoutRoutineAddDailyRoutineService,
+        private readonly workoutFindService: WorkoutRoutineFindService) { }
 
     public async create(body: DailyRoutine, id: string): Promise<DailyRoutine> {
-        const workout = await this.workoutFindByPersonIdService.findByPersonId(id)
+        const workout = await this.workoutFindService.find(id)
         const dailyRoutine = new DailyRoutine(body.name)
         await this.model.save(dailyRoutine)
         await this.workoutAddDailyRoutineService.add(dailyRoutine, workout)
