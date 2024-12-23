b2dafed4302afb02acbd5a4d476b073f806739af
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-12-23 11:31:46 -0300

    modify params request

--- src/services/workoutroutine.create.service.ts
+++ src/services/workoutroutine.create.service.ts
@@ -2,24 +2,22 @@ import { Injectable, NotAcceptableException, NotFoundException } from "@nestjs/c
 import { InjectRepository } from "@nestjs/typeorm";
 import { WorkoutRoutine } from "src/models/workoutroutine.model";
 import { Repository } from "typeorm";
-import { PersonFindService } from "./person.find.service";
 import { PersonUpdateService } from "./person.update.service";
+import { Person } from "src/models/person.model";
 
 @Injectable()
 export class WorkoutRoutineCreateService {
 
     constructor(@InjectRepository(WorkoutRoutine) private model: Repository<WorkoutRoutine>,
-        private readonly personFindService: PersonFindService,
         private readonly personUpdateService: PersonUpdateService) { }
 
-    public async create(id: string): Promise<WorkoutRoutine> {
-        const person = await this.personFindService.find(id)
+    public async create(person: Person): Promise<WorkoutRoutine> {
         if (person.workoutRoutine != null)
             throw new NotAcceptableException('Person already have workout routine')
         const workout = new WorkoutRoutine()
         person.workoutRoutine = workout
         await this.model.save(workout)
-        await this.personUpdateService.update(id, person)
+        await this.personUpdateService.update(person.id, person)
         return workout
     }
 }
