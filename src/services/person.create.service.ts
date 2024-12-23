238895cb512ef4641b5e8f32d54daa29cc650970
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-12-23 11:31:19 -0300

    modify -> person and workout routine are created in same time

--- src/services/person.create.service.ts
+++ src/services/person.create.service.ts
@@ -2,13 +2,17 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Person } from "src/models/person.model";
 import { Repository } from "typeorm";
+import { WorkoutRoutineCreateService } from "./workoutroutine.create.service";
 
 @Injectable()
 export class PersonCreateService {
 
-    constructor(@InjectRepository(Person) private model: Repository<Person>) { }
+    constructor(@InjectRepository(Person) private model: Repository<Person>,
+        private readonly workoutRoutineCreateService: WorkoutRoutineCreateService) { }
 
     public async create(body: Person): Promise<Person> {
-        return await this.model.save(body)
+        const person = await this.model.save(body)
+        this.workoutRoutineCreateService.create(person)
+        return person
     }
 }
