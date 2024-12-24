8fe909302095046c23ba1c3680dbe3c66278b8ad
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-12-24 11:19:39 -0300

    modify create bodybuilding exercise in dailyroutine

---
+++ src/services/dailyroutine.createbodybuilding.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { DailyRoutineUpdateService } from "./dailyroutine.update.service";
+import { BodybuildingCreateService } from "./bodybuilding.create.service";
+import { BodyBuildingExercise } from "src/models/bodybuildingexercise.model";
+import { DailyRoutineFindService } from "./dailyroutine.find.service";
+
+
+@Injectable()
+export class DailyRoutineCreateBodybuildingService {
+
+    constructor(private readonly bodybuildingCreateService: BodybuildingCreateService,
+        private readonly dailyRoutineUpdateService: DailyRoutineUpdateService,
+        private readonly dailyRoutineFindService: DailyRoutineFindService) { }
+
+    public async add(id: string, body: BodyBuildingExercise) {
+        const dailyRoutine = await this.dailyRoutineFindService.find(id)
+        if (!dailyRoutine)
+            throw new NotFoundException('Daily routine not found')
+        const bodybuilding = await this.bodybuildingCreateService.create(body)
+        dailyRoutine.bodybuildingExercises.push(bodybuilding)
+        await this.dailyRoutineUpdateService.update(dailyRoutine)
+        return bodybuilding
+    }
+}   
