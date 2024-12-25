3fa3e4b325916e35ce03a6b90f0db3ed22598467
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-12-25 11:14:24 -0300

    modify person create service -> password are hashed

--- src/services/person.create.service.ts
+++ src/services/person.create.service.ts
@@ -3,14 +3,17 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Person } from "src/models/person.model";
 import { Repository } from "typeorm";
 import { WorkoutRoutineCreateService } from "./workoutroutine.create.service";
+import { HashPasswordUtility } from "src/utils/hash.password.util";
 
 @Injectable()
 export class PersonCreateService {
 
     constructor(@InjectRepository(Person) private model: Repository<Person>,
-        private readonly workoutRoutineCreateService: WorkoutRoutineCreateService) { }
+        private readonly workoutRoutineCreateService: WorkoutRoutineCreateService,
+        private readonly hashPassword: HashPasswordUtility) { }
 
     public async create(body: Person): Promise<Person> {
+        body.password = await this.hashPassword.hash(body.password)
         const person = await this.model.save(body)
         this.workoutRoutineCreateService.create(person)
         return person
