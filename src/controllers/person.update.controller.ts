cb1dff0aa4d0f26d546bcc0efd0b911447826dd8
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-12-26 22:04:03 -0300

    modify logic

--- src/controllers/person.update.controller.ts
+++ src/controllers/person.update.controller.ts
@@ -1,14 +1,17 @@
 import { Body, Controller, NotFoundException, Param, Put } from "@nestjs/common";
 import { Person } from "src/models/person.model";
 import { PersonUpdateService } from "src/services/person.update.service";
+import { PersonFindService } from '../services/person.find.service';
 
 @Controller('/person')
 export class PersonUpdateController {
 
-    constructor(private readonly service: PersonUpdateService) { }
+    constructor(private readonly personFindService: PersonFindService,
+        private readonly personUpdateService: PersonUpdateService) { }
 
     @Put('/:id')
     public async update(@Param('id') id: string, @Body() body: Person): Promise<Person> {
-        return this.service.update(id, body)
+        if (await this.personFindService.find(id))
+            return this.personUpdateService.update(body)
     }
 }
