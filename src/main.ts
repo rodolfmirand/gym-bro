5f3c5ce0ca893ce2f11e9fd7bc9ed8ab229704ce
Eduardo Octávio <eeuardooctavio@gmail.com>
2024-12-27 00:43:21 -0300

    feat

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,11 @@ async function bootstrap() {
   app.setGlobalPrefix('/gymbro');
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
