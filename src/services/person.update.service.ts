7d43eee9c3258a5c4644014f81ad6cfeaa18cee5
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-12-27 20:53:23 -0300

    modify params

--- src/services/person.update.service.ts
+++ src/services/person.update.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from "@nestjs/typeorm";
+import { PersonUpdateDTO } from 'src/dtos/response/person.update.dto';
 import { Person } from "src/models/person.model";
 import { Repository } from "typeorm";
 
@@ -8,7 +9,7 @@ export class PersonUpdateService {
 
     constructor(@InjectRepository(Person) private model: Repository<Person>) { }
 
-    public async update(person: Person): Promise<Person> {
+    public async update(person: PersonUpdateDTO): Promise<PersonUpdateDTO> {
         return this.model.save(person)
     }
 }
