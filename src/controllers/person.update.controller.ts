34db01678a1f5c8612e6cfd1ec6d09e670774e2a
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-12-27 21:54:43 -0300

    modify person modify controller

--- src/controllers/person.update.controller.ts
+++ src/controllers/person.update.controller.ts
@@ -1,19 +1,21 @@
 import { Body, Controller, NotFoundException, Param, ParseUUIDPipe, Put, UseGuards } from "@nestjs/common";
+import { Person } from "src/models/person.model";
 import { PersonUpdateService } from "src/services/person.update.service";
 import { PersonFindService } from '../services/person.find.service';
 import { AuthGuard } from "src/auth/auth.guard";
+import { PersonRequestDTO } from "src/dtos/request/person.request.dto";
 import { PersonUpdateDTO } from "src/dtos/response/person.update.dto";
 
-@Controller('person')
+@Controller('/person')
 export class PersonUpdateController {
 
     constructor(private readonly personFindService: PersonFindService,
         private readonly personUpdateService: PersonUpdateService) { }
 
     @UseGuards(AuthGuard)
-    @Put(':id')
-    public async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() body: PersonUpdateDTO): Promise<PersonUpdateDTO> {
-        if (await this.personFindService.find(id))
-            return this.personUpdateService.update(body)
+    @Put('/:id')
+    public async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() body: PersonUpdateDTO): Promise<string> {
+        const person = await this.personFindService.find(id)
+        return this.personUpdateService.update(person, body)
     }
 }
