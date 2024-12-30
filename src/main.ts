96d26bc3a62d5a3f2a48cd5f2ffd0e80e24e98ad
Eduardo Octávio <eeuardooctavio@gmail.com>
2024-12-30 20:36:03 -0300

    feat

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   await app.listen(process.env.PORT ?? 8080);
 }
