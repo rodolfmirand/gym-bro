e03386262c8dfd451bd4a838674facac95c8b3a2
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2024-12-30 08:10:23 -0300

    add logout endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,13 @@
-import { Body, Controller, Post, UnauthorizedException } from "@nestjs/common";
+import { Body, Controller, Post, Req, UnauthorizedException } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { LogoutService } from "./logout.service";
 
 @Controller('auth')
 export class AuthController {
 
-    constructor(private readonly service: AuthService) { }
+    constructor(private readonly service: AuthService,
+        private readonly logoutService: LogoutService
+    ) { }
 
     @Post('login')
     public async login(@Body('username') username: string, @Body('password') password: string) {
@@ -12,5 +15,15 @@ export class AuthController {
         if (!person)
             throw new UnauthorizedException('Invalid username or password')
         return this.service.login(person)
+    }
+
+    @Post('logout')
+    public async logout(@Req() request: any): Promise<string> {
+        const token = request.headers.authorization?.split(' ')[1];
+        if (!token)
+            return 'No token provided'
+
+        await this.logoutService.addToBlacklist(token)
+        return 'Logged out successfully'
     }
 }
