840dc49ecf019cda3f169a13d4173fdec12e919f
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-01-03 10:19:49 -0300

    add expose decorator

--- src/dtos/request/cardio.request.dto.ts
+++ src/dtos/request/cardio.request.dto.ts
@@ -1,24 +1,30 @@
+import { Expose } from "class-transformer";
 import { IsEnum, IsNotEmpty, IsOptional, IsString, IsUrl, ValidateIf } from "class-validator";
 import { CardioExerciseEnum } from "src/models/enum/cardioexercise.enum";
 
 export class CardioRequestDTO {
-
+    
+    @Expose()
     @IsNotEmpty()
     @IsEnum(CardioExerciseEnum)
     cardioExercise: CardioExerciseEnum
 
+    @Expose()
     @IsString()
     @IsOptional()
     description: string
-    
+
+    @Expose()
     @IsString()
     @IsOptional()
     equipment: string
     
+    @Expose()
     @IsString()
     @IsOptional()
     time: string
     
+    @Expose()
     @ValidateIf((obj)=> obj.videoUrl !== null && obj.videoUrl !== "")
     @IsOptional()
     @IsUrl()
