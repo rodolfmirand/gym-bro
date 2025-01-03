0954009b50d0353675b75eaeea3c626b23a65a9a
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-01-03 10:21:24 -0300

    add expose decorator

--- src/models/cardioexercise.model.ts
+++ src/models/cardioexercise.model.ts
@@ -1,25 +1,32 @@
 import { Exercise } from "./abstract/exercise.abstract";
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { CardioExerciseEnum } from "./enum/cardioexercise.enum";
+import { Expose } from "class-transformer";
 
 @Entity()
 export class CardioExercise extends Exercise {
 
+    @Expose()
     @PrimaryGeneratedColumn('uuid')
     id: string
 
+    @Expose()
     @Column()
     cardioExercise: CardioExerciseEnum
 
+    @Expose()
     @Column({ length: 200 })
     description: string
 
+    @Expose()
     @Column({ length: 50 })
     equipment: string
-
+    
+    @Expose()
     @Column()
     time: string
 
+    @Expose()
     @Column()
     videoUrl: string
 
