830d8c358fc36b69f4737e7eae4363a9356d1f2b
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-01-06 12:28:59 -0300

    remove unnused imports

--- src/services/dailyroutine.findbypersonid.service.ts
+++ src/services/dailyroutine.findbypersonid.service.ts
@@ -1,14 +1,11 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
 import { DailyRoutine } from "src/models/dailyroutine.model";
-import { Repository } from "typeorm";
 import { PersonFindService } from "./person.find.service";
 
 @Injectable()
 export class DailyRoutineFindByPersonIdService {
 
-    constructor(@InjectRepository(DailyRoutine) private model: Repository<DailyRoutine>,
-        private readonly personFindService: PersonFindService) { }
+    constructor(private readonly personFindService: PersonFindService) { }
 
     public async findByPersonId(id: string): Promise<DailyRoutine[]> {
         const person = await this.personFindService.find(id)
