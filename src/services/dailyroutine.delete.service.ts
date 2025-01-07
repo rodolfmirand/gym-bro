b2a860372f5bd87b6e6f947cb1688b1c56d52f14
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-01-07 20:21:17 -0300

    modify logic

--- src/services/dailyroutine.delete.service.ts
+++ src/services/dailyroutine.delete.service.ts
@@ -2,17 +2,35 @@ import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { DailyRoutine } from "src/models/dailyroutine.model";
 import { Repository } from "typeorm";
+import { WorkoutRoutineFindService } from "./workoutroutine.find.service";
+import { DailyRoutineUpdateService } from "./dailyroutine.update.service";
+import { GenerateLetter } from 'src/utils/generate.letter.util';
 
 @Injectable()
 export class DailyRoutineDeleteService {
 
-    constructor(@InjectRepository(DailyRoutine) private model: Repository<DailyRoutine>) { }
+    constructor(@InjectRepository(DailyRoutine) private model: Repository<DailyRoutine>,
+        private readonly dailyRoutineUpdateService: DailyRoutineUpdateService,
+        private readonly workoutRoutineFindService: WorkoutRoutineFindService
+    ) { }
 
-    public async delete(id: string): Promise<string> {
+    public async delete(id: string, idWorkoutRoutine: string): Promise<string> {
         const result = await this.model.delete(id);
         if (result.affected === 0) {
             throw new NotFoundException('Daily routine not found')
         }
+        const workout = await this.workoutRoutineFindService.find(idWorkoutRoutine)
+        const dailyRoutine = workout.dailyRoutine
+        let counter = 65 
+        dailyRoutine.forEach((daily) => {
+            if (dailyRoutine.indexOf(daily) === 0) {
+                daily.name = 'A'
+            } else {
+                daily.name = GenerateLetter.generateNextLetter(String.fromCharCode(counter))
+                counter++
+            }
+            this.dailyRoutineUpdateService.update(daily)
+        })
         return 'Daily routine deleted successfully'
     }
 }
