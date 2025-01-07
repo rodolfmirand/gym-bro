7e48f2942606b592c25c96aa41df33776244146a
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-01-07 18:46:30 -0300

    generalization of the method

--- src/controllers/exercise.delete.controller.ts
+++ src/controllers/exercise.delete.controller.ts
@@ -2,21 +2,17 @@ import { Controller, Delete, Param, ParseUUIDPipe, UseGuards } from "@nestjs/com
 import { AuthGuard } from "src/auth/auth.guard";
 import { BodyBuildingDeleteService } from "src/services/bodybuilding.delete.service";
 import { CardioDeleteService } from "src/services/cardio.delete.service";
+import { ExerciseDeleteService } from "src/services/exercise.delete.service";
 
 @Controller('exercise')
 export class ExerciseDeleteController {
 
-    constructor(private readonly bodybuildingService: BodyBuildingDeleteService, private readonly cardioService: CardioDeleteService) { }
+    constructor(private readonly service: ExerciseDeleteService,
+        private readonly bodybuildingService: BodyBuildingDeleteService, private readonly cardioService: CardioDeleteService) { }
 
     @UseGuards(AuthGuard)
-    @Delete('/bodybuilding/:id')
-    public async deleteBodybuilding(@Param('id', new ParseUUIDPipe()) id: string): Promise<string> {
-        return this.bodybuildingService.delete(id)
-    }
-
-    @UseGuards(AuthGuard)
-    @Delete('cardio/:id')
-    public async deleteCardio(@Param('id', new ParseUUIDPipe()) id: string): Promise<string> {
-        return this.cardioService.delete(id)
+    @Delete(':id')
+    public async delete(@Param('id', new ParseUUIDPipe()) id: string): Promise<string> {
+        return this.service.delete(id)
     }
 }
