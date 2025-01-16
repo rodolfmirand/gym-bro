b161f5729bacf8875239771c7fddd820ea86ec66
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-01-15 22:09:02 -0300

    modify imports path

--- src/modules/exercise.module.ts
+++ src/modules/exercise.module.ts
@@ -1,27 +1,27 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { CardioCreateController } from "src/controllers/cardio.create.controller";
-import { ExerciseDeleteController } from "src/controllers/exercise.delete.controller";
-import { ExerciseFindController } from "src/controllers/exercise.find.controller";
-import { CardioFindAllController } from "src/controllers/cardio.findall.controller";
-import { ExerciseUpdateController } from "src/controllers/exercise.update.controller";
 import { BodyBuildingExercise } from "src/models/bodybuildingexercise.model";
 import { CardioExercise } from "src/models/cardioexercise.model";
-import { BodybuildingCreateService } from "src/services/bodybuilding.create.service";
-import { BodyBuildingDeleteService } from "src/services/bodybuilding.delete.service";
-import { BodybuildingFindService } from "src/services/bodybuilding.find.service";
-import { BodybuildingFindAllService } from "src/services/bodybuilding.findall.service";
-import { BodybuildingUpdateService } from "src/services/bodybuilding.update.service";
-import { CardioCreateService } from "src/services/cardio.create.service";
-import { CardioDeleteService } from "src/services/cardio.delete.service";
-import { CardioFindService } from "src/services/cardio.find.service";
-import { CardioFindAllService } from "src/services/cardio.findall.service";
-import { CardioUpdateService } from "src/services/cardio.update.service";
-import { ExerciseDeleteService } from '../services/exercise.delete.service';
-import { ExerciseUpdateService } from "src/services/exercise.update.service";
-import { BodybuildingCreateController } from "src/controllers/bodybuilding.create.controller";
-import { BodybuildingFindAllController } from "src/controllers/bodybuilding.findall.controller";
-import { ExerciseFindService } from "src/services/exercise.find.service";
+import { BodybuildingCreateService } from "src/services/exercise/bodybuilding/bodybuilding.create.service";
+import { BodyBuildingDeleteService } from "src/services/exercise/bodybuilding/bodybuilding.delete.service";
+import { BodybuildingFindService } from "src/services/exercise/bodybuilding/bodybuilding.find.service";
+import { BodybuildingFindAllService } from "src/services/exercise/bodybuilding/bodybuilding.findall.service";
+import { BodybuildingUpdateService } from "src/services/exercise/bodybuilding/bodybuilding.update.service";
+import { CardioCreateService } from "src/services/exercise/cardio/cardio.create.service";
+import { CardioDeleteService } from "src/services/exercise/cardio/cardio.delete.service";
+import { CardioFindService } from "src/services/exercise/cardio/cardio.find.service";
+import { CardioFindAllService } from "src/services/exercise/cardio/cardio.findall.service";
+import { CardioUpdateService } from "src/services/exercise/cardio/cardio.update.service";
+import { ExerciseDeleteService } from '../services/exercise/exercise.delete.service';
+import { ExerciseUpdateService } from "src/services/exercise/exercise.update.service";
+import { ExerciseFindService } from "src/services/exercise/exercise.find.service";
+import { CardioCreateController } from "src/controllers/exercise/cardio/cardio.create.controller";
+import { ExerciseFindController } from "src/controllers/exercise/exercise.find.controller";
+import { ExerciseDeleteController } from "src/controllers/exercise/exercise.delete.controller";
+import { ExerciseUpdateController } from "src/controllers/exercise/exercise.update.controller";
+import { CardioFindAllController } from "src/controllers/exercise/cardio/cardio.findall.controller";
+import { BodybuildingCreateController } from "src/controllers/exercise/bodybuilding/bodybuilding.create.controller";
+import { BodybuildingFindAllController } from "src/controllers/exercise/bodybuilding/bodybuilding.findall.controller";
 
 @Module({
     imports: [TypeOrmModule.forFeature([BodyBuildingExercise, CardioExercise])],
--- src/modules/workoutroutine.module.ts
+++ src/modules/workoutroutine.module.ts
@@ -3,12 +3,12 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { WorkoutRoutineFindAllController } from "src/controllers/workoutroutine.findall.controller";
 import { WorkoutRoutine } from "src/models/workoutroutine.model";
 import { PersonModule } from "./person.module";
-import { WorkoutRoutineCreateService } from "src/services/workoutroutine.create.service";
-import { WorkoutRoutineFindAllService } from "src/services/workoutroutine.findall.service";
-import { WorkoutRoutineFindByPersonIdService } from "src/services/workoutroutine.findbypersonid.service";
+import { WorkoutRoutineCreateService } from "src/services/routines/workoutroutine/workoutroutine.create.service";
+import { WorkoutRoutineFindAllService } from "src/services/routines/workoutroutine/workoutroutine.findall.service";
+import { WorkoutRoutineFindByPersonIdService } from "src/services/routines/workoutroutine/workoutroutine.findbypersonid.service";
 import { WorkoutRoutineFindByPersonIdController } from "src/controllers/workoutroutine.findbypersonid.controller";
-import { WorkoutRoutineUpdateService } from "src/services/workoutroutine.update.service";
-import { WorkoutRoutineFindService } from "src/services/workoutroutine.find.service";
+import { WorkoutRoutineUpdateService } from "src/services/routines/workoutroutine/workoutroutine.update.service";
+import { WorkoutRoutineFindService } from "src/services/routines/workoutroutine/workoutroutine.find.service";
 import { DailyRoutineModule } from "./dailyroutine.module";
 import { Person } from "src/models/person.model";
 
