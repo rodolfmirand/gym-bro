d913fd6765edbcea7258bb3e53cdb6457537fb4c
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-01-15 21:37:01 -0300

    delete exercises controllers

--- src/controllers/exercise.find.controller.ts
+++ src/controllers/exercise.find.controller.ts
@@ -1,21 +1,14 @@
 import { Controller, Get, Param, ParseUUIDPipe } from "@nestjs/common";
-import { BodyBuildingExercise } from "src/models/bodybuildingexercise.model";
 import { CardioExercise } from "src/models/cardioexercise.model";
-import { BodybuildingFindService } from "src/services/bodybuilding.find.service";
-import { CardioFindService } from "src/services/cardio.find.service";
+import { ExerciseFindService } from "src/services/exercise.find.service";
 
 @Controller('exercise')
 export class ExerciseFindController {
 
-    constructor(private readonly bodybuildingService: BodybuildingFindService, private readonly cardioService: CardioFindService) { }
+    constructor(private readonly service: ExerciseFindService) { }
 
-    @Get('bodybuilding/:id')
-    public async findBodybuilding(@Param('id', new ParseUUIDPipe()) id: string): Promise<BodyBuildingExercise> {
-        return this.bodybuildingService.find(id)
-    }
-
-    @Get('cardio/:id')
-    public async findCardio(@Param('id', new ParseUUIDPipe()) id: string): Promise<CardioExercise> {
-        return this.cardioService.find(id)
+    @Get(':id')
+    public async find(@Param('id', new ParseUUIDPipe()) id: string): Promise<CardioExercise> {
+        return this.service.find(id)
     }
 }
