20152c42ef42afaf252c2a6f1f3eaa5ddcd5cafd
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-01-15 21:56:41 -0300

    add new folders organization

---
+++ src/controllers/exercise/exercise.find.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Param, ParseUUIDPipe } from "@nestjs/common";
+import { CardioExercise } from "src/models/cardioexercise.model";
+import { ExerciseFindService } from "src/services/exercise/exercise.find.service";
+
+@Controller('exercise')
+export class ExerciseFindController {
+
+    constructor(private readonly service: ExerciseFindService) { }
+
+    @Get(':id')
+    public async find(@Param('id', new ParseUUIDPipe()) id: string): Promise<any> {
+        return this.service.find(id)
+    }
+}
