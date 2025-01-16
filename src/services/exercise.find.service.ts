9d72526b66d4947b1862fc6e77a794784df62404
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-01-15 21:40:38 -0300

    add exercise find service

---
+++ src/services/exercise.find.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { BodyBuildingExercise } from "src/models/bodybuildingexercise.model";
+import { CardioExercise } from "src/models/cardioexercise.model";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class ExerciseFindService {
+
+    constructor(@InjectRepository(CardioExercise) private cardioFindService: Repository<CardioExercise>,
+        @InjectRepository(BodyBuildingExercise) private bodybuildingFindService: Repository<BodyBuildingExercise>
+    ) { }
+
+    public async find(id: string): Promise<any> {
+        const bodybuilding = await this.bodybuildingFindService.findOne({ where: { id } })
+        if (bodybuilding)
+            return bodybuilding
+        const cardio = await this.cardioFindService.findOne({ where: { id } })
+        if (cardio)
+            return cardio
+        return { status: 'Exercise not found' }
+    }
+}
