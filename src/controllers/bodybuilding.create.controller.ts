863e1d6f8edf0056f982cc9524aec9f8b05e63b4
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-01-15 21:39:39 -0300

    add bodybuilding create controller

---
+++ src/controllers/bodybuilding.create.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "src/auth/auth.guard";
+import { BodybuildingRequestDTO } from "src/dtos/request/bodybuilding.request.dto";
+import { BodyBuildingExercise } from "src/models/bodybuildingexercise.model";
+import { BodybuildingCreateService } from "src/services/bodybuilding.create.service";
+
+@Controller('exercise')
+export class BodybuildingCreateController {
+
+    constructor(private readonly service: BodybuildingCreateService) { }
+
+    @UseGuards(AuthGuard)
+    @Post('bodybuilding')
+    public async create(@Body() body: BodybuildingRequestDTO): Promise<BodyBuildingExercise> {
+        return this.service.create(body)
+    }
+}
