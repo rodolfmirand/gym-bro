d7dd13b5d32218cdda762a9c116d5ca080233b93
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-01-15 21:40:09 -0300

    add cardio create controller

---
+++ src/controllers/cardio.create.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "src/auth/auth.guard";
+import { CardioRequestDTO } from "src/dtos/request/cardio.request.dto";
+import { CardioExercise } from "src/models/cardioexercise.model";
+import { CardioCreateService } from "src/services/cardio.create.service";
+
+@Controller('exercise')
+export class CardioCreateController {
+
+    constructor(private readonly service: CardioCreateService) { }
+
+    @UseGuards(AuthGuard)
+    @Post('cardio')
+    public async create(@Body() body: CardioRequestDTO): Promise<CardioExercise> {
+        return this.service.create(body)
+    }
+}
