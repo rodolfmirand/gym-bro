62f4bf575517aa5026488f07677cf9a785301528
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-01-15 21:38:58 -0300

    modify exercise find controller

--- src/controllers/exercise.find.controller.ts
+++ src/controllers/exercise.find.controller.ts
@@ -8,7 +8,7 @@ export class ExerciseFindController {
     constructor(private readonly service: ExerciseFindService) { }
 
     @Get(':id')
-    public async find(@Param('id', new ParseUUIDPipe()) id: string): Promise<CardioExercise> {
+    public async find(@Param('id', new ParseUUIDPipe()) id: string): Promise<any> {
         return this.service.find(id)
     }
 }
