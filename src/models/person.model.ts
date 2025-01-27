13a41ea9bfb4fb6700fd7abc3211062d652b4144
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-01-27 10:40:04 -0300

    remove expose decorator

--- src/models/person.model.ts
+++ src/models/person.model.ts
@@ -5,19 +5,15 @@ import { Exclude, Expose } from 'class-transformer';
 @Entity()
 export class Person {
 
-    @Expose()
     @PrimaryGeneratedColumn('uuid')
     id: string
 
-    @Expose()
     @Column({ length: 100 })
     name: string
-
-    @Expose()
+    
     @Column({length: 12, unique: true})
     username: string
 
-    @Expose()
     @Column({ length: 100, unique: true })
     email: string
 
@@ -25,19 +21,15 @@ export class Person {
     @Column({ length: 16 })
     password: string
 
-    @Expose()
     @Column()
     height: number
 
-    @Expose()
     @Column()
     weight: number
 
-    @Expose()
     @Column()
     birthDate: string
 
-    @Expose()
     @OneToOne(() => WorkoutRoutine, workoutRoutine => workoutRoutine.person, { cascade: true, onDelete: 'CASCADE' })
     @JoinColumn()
     workoutRoutine: WorkoutRoutine
