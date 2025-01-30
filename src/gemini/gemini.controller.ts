2a9d5061a6537094be176493ec7203aed9ef20e3
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-01-30 20:11:11 -0300

    modify gemini controller

--- src/gemini/gemini.controller.ts
+++ src/gemini/gemini.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Param, ParseUUIDPipe, Post } from '@nestjs/common';
 import { GeminiService } from './gemini.service';
-import { Restrictions } from './models/dtos/restrictions.model';
+import { Restrictions } from './models/restrictions.model';
+import { GeminiResponseDTO } from './models/dtos/gemini.response.dto';
 
 @Controller('gemini')
 export class GeminiController {
@@ -8,7 +9,7 @@ export class GeminiController {
     constructor(private readonly geminiService: GeminiService) { }
 
     @Post(':id')
-    public generateText(@Param('id', new ParseUUIDPipe) id: string, @Body() restrictions: Restrictions): Promise<string> {
+    public async generateText(@Param('id', new ParseUUIDPipe) id: string, @Body() restrictions: Restrictions): Promise<GeminiResponseDTO> {
         return this.geminiService.generateText(id, restrictions)
     }
 }
