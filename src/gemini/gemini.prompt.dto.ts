2a92807db39b82a1d4fb79da962f91ad3a120ffa
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-01-30 20:12:12 -0300

    delete prompt request class

--- src/gemini/gemini.prompt.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Expose } from "class-transformer";
-import { IsNotEmpty, IsString } from "class-validator";
-
-export class PromptDTO {
-    @Expose()
-    @IsNotEmpty()
-    @IsString()
-    message: string
-}
