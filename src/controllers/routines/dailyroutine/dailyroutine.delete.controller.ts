f091b59d3303224b1317ca7bfb4ebe7899f8fa8f
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-02-22 12:45:55 -0300

    resolve conflits

--- src/controllers/routines/dailyroutine/dailyroutine.delete.controller.ts
+++ src/controllers/routines/dailyroutine/dailyroutine.delete.controller.ts
@@ -7,12 +7,7 @@ export class DailyRoutineDeleteController {
     constructor(private readonly service: DailyRoutineDeleteService) { }
 
     @Delete(':id/:idPerson')
-<<<<<<< HEAD
     public async delete(@Param('id', new ParseUUIDPipe()) id: string, @Param('idW', new ParseUUIDPipe()) idPerson: string): Promise<any> {
         return this.service.delete(id, idPerson)
-=======
-    public async delete(@Param('id', new ParseUUIDPipe()) id: string, @Param('idPerson', new ParseUUIDPipe()) idPerson: string): Promise<any> {
-        return this.dailyRoutineDeleteService.delete(id, idPerson)
->>>>>>> e4d1509f07e98edc210a8af47854b953860de841
     }
 }
