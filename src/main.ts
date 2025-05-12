b4721211a822ea502803e1d43df9e89e011c3ae6
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-12 18:04:20 -0300

    refactor origin url

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     }
   }));
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
