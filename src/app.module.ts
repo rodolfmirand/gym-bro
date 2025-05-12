8f1463069c69b5c0a7a99b25ab4c2930a1454c61
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-05-12 18:17:10 -0300

    refactor database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,14 +10,24 @@ import { GeminiModule } from './gemini/gemini.module';
 
 dotenv.config();
 
+// @Module({
+//   imports: [PersonModule, ExerciseModule, TypeOrmModule.forRoot({
+//     type: 'postgres',
+//     host: process.env.DB_HOST,
+//     port: parseInt(process.env.DB_PORT, 10),
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+//     synchronize: true,
+//     entities: ["dist/**/*.model.js"],
+//   })],
+// })
+
+
 @Module({
   imports: [GeminiModule, AuthModule, PersonModule, ExerciseModule, WorkoutRoutineModule, DailyRoutineModule, TypeOrmModule.forRoot({
-    "host": process.env.DB_HOST,
-    "port": parseInt(process.env.DB_PORT, 10),
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "type": "postgres",
+    "database": "./database.sql",
+    "type": "sqlite",
     "synchronize": true,
     "entities": ["dist/**/*.model.js"]
   }), GeminiModule],
